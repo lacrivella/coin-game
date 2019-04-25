e161fb72c166c3ff41484c4a3381fbf193d77379
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-04-25 10:13:37 -0700

    fixed image issue. had to change name files to match function

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import flipCoin from './flip-coin.js';
 const flipButton = document.getElementById('flip-it');
 const headChoice = document.getElementById('head-choice');
 const coinImage = document.getElementById('coin-image');
+const winResults = document.getElementById('wins');
+const lossResult = document.getElementById('loss');
 
 //initialize things?
 
@@ -12,19 +14,25 @@ const coinImage = document.getElementById('coin-image');
 flipButton.addEventListener('click', () => {
     const randomNum = Math.random();
     const flip = flipCoin(randomNum);
-    const imageSource = './assets/' + flip + '.jpg';
-    coinImage.src = imageSource;
-    let guess = '';
 
+    //trying to make image flip
+    const imageSource = 'assets/' + flip + '.jpg';
+    coinImage.src = imageSource;
+
+    let guess = '';
     if(headChoice.checked) {
         guess = 'head';
     }
     else {
-      guess = 'tails';  
+        guess = 'tails';  
     }
 
+    const Correct = flip === guess;
+
    if(flip === guess) {
-       console.log('winner winner chicken dinner!');
+       console.log('winner winner chicken dinner')
+       wins++;
+       winResults.textContent = 'Wins' + wins;
    }
    else {
        console.log('you lose. sucks to suck');
