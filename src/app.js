f3987873e80bf70df6637af83b9ca05666e5bcd3
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-04-24 19:55:26 -0700

    wired button to console

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log('hello');
+// reference DOM elements needed
+const flipButton = document.getElementById('flip-it');
+
+//initialize things?
+
+//add event listeners
+flipButton.addEventListener('click', () => {
+    console.log('click click'); 
+});
