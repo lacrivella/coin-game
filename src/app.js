2a48359da2f6bd095b936a31e940ce7c6e58eb4f
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-04-25 08:45:23 -0700

    created if else statements to show message in the console when guess is correct

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import flipCoin from './flip-coin.js';
 
 // reference DOM elements needed
 const flipButton = document.getElementById('flip-it');
+const headChoice = document.getElementById('head-choice');
+const coinImage = document.getElementById('coin-image');
 
 //initialize things?
 
@@ -10,5 +12,20 @@ const flipButton = document.getElementById('flip-it');
 flipButton.addEventListener('click', () => {
     const randomNum = Math.random();
     const flip = flipCoin(randomNum);
-    console.log(flip);
+    
+    let guess = '';
+
+    if(headChoice.checked) {
+        guess = 'head';
+    }
+    else {
+      guess = 'tails';  
+    }
+
+   if(flip === guess) {
+       console.log('winner winner chicken dinner!');
+   }
+   else {
+       console.log('you lose. sucks to suck');
+   }
 });
