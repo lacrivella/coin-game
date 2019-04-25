6da4beed2f6d2a17f01057a5cb8706b18166112c
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-04-24 20:23:38 -0700

    tested function and passed TDD for heads when greater than ot equal to .5

--- test/tests.js
+++ test/tests.js
@@ -5,6 +5,7 @@ function flipCoin(number) {
     if(number < .5) {
         return 'tails';
     }
+    return 'heads';
 }
 
 
@@ -13,6 +14,20 @@ test('get test for tails when less than .5', function(assert) {
     // Set up your parameters and expectations
     const number = .49;
     const expected = 'tails';
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = flipCoin(number);
+
+    //Assert
+    assert.equal(result, expected);
+});
+
+test('get test for heads when greater than or equal to .5', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const number = .5;
+    const expected = 'heads';
 
     //Act 
     // Call the function you're testing and set the result to a const
